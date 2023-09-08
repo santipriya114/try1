5b880df74c5770a63d01df417c8d703b37f98989
Admin <admin.push@hackerrank.workspace>
2023-09-08 09:37:24 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
+   const [restarts,isrestarts]=useState(true);
+   const [pres,ispre]=useState(false);
+   const [nexts,isnext]=useState(false);
+    
+   function restarthandler(){
+     if(restarts==true){
+       
+     }
+   }
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
+                <button data-testid="button-restart" className="small outlined" onClick={restarthandler}>Restart</button>
                 <button data-testid="button-prev" className="small">Prev</button>
                 <button data-testid="button-next" className="small">Next</button>
             </div>
